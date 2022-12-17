09e6de81a1f88e4d879c37fed80eb06900954186
Aswanth P <Aswanth@NTP-LAP-734.chn.nestgroup.net>
2022-12-17 09:40:38 +0530

    leave2nd

---
+++ src/main/java/com/nest/leave_2nd/Leave2ndApplication.java
@@ -0,0 +1,13 @@
+package com.nest.leave_2nd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Leave2ndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Leave2ndApplication.class, args);
+	}
+
+}
